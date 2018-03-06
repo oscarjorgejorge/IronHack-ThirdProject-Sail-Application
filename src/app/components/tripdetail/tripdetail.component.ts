792c3a37a54dd2b71e399c2ff7d285bd6ea188dc
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-03-06 13:41:03 +0100

    component trip detail linked with page-component-trip-detail

--- src/app/components/tripdetail/tripdetail.component.ts
+++ src/app/components/tripdetail/tripdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tripdetail',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tripdetail.component.css']
 })
 export class TripdetailComponent implements OnInit {
+  @Input() trip: object;
 
   constructor() { }
 
